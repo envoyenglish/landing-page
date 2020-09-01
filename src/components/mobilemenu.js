370a36306fd0c03085ef158a5c52172e082b9cc8
Sue Li <sueli@post.harvard.edu>
2020-09-01 20:50:25 +0200

    close mobile menu on navigation

--- src/components/mobilemenu.js
+++ src/components/mobilemenu.js
@@ -12,15 +12,34 @@ const iconStyle={
 }
 
 class MobileMenu extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
+  }
+  
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
   render () {
     return (
-      <Menu disableAutoFocus>
-        <Link to='/'><img src={logo} alt="logo" style={iconStyle}/></Link>
-        <Link to='/register' id="free-trial-menu-item">FREE Trial Class</Link>
+      <Menu disableAutoFocus
+        isOpen = {this.state.menuOpen}
+        onStateChange={(state) => this.handleStateChange(state)}
+      >
+        <Link to='/' onClick={() => this.closeMenu()}><img src={logo} alt="logo" style={iconStyle}/></Link>
+        <Link to='/register' onClick={() => this.closeMenu()} id="free-trial-menu-item">FREE Trial Class</Link>
         <h4>Courses</h4>
-        <Link to='/business-english-masterclass-intensive'>Business English Masterclass</Link>
-        <Link to='/business-communication-course'>Business Communication Course</Link>
-        <Link to='/discussion-course-a2'>Conversation Course</Link>
+        <Link to='/business-english-masterclass-intensive' onClick={() => this.closeMenu()}>Business English Masterclass</Link>
+        <Link to='/business-communication-course' onClick={() => this.closeMenu()}>Business Communication</Link>
+        <Link to='/discussion-course-a2' onClick={() => this.closeMenu()}>Conversation Course</Link>
       </Menu>
     );
   }
