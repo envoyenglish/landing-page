52204af896f18752592b17b0a8a40022742a6d7c
Sue Li <sueli@post.harvard.edu>
2020-09-27 20:49:18 +0200

    refactor header to functional component

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import {Logo, Navbar, Button, NavbarButtons, HamburgerMenu, DesktopMenu} from '../styles/ui-components';
 import logo from '../assets/logo.png';
@@ -20,59 +20,51 @@ const iconStyle={
   'minWidth': '100%'
 }
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      language: 'EN'  
-    }
-  }
+function Header(props) {
 
-  render() {
-    let buttonText = 'Prueba gratis';
-    if (this.props.language === 'EN') {
+  let buttonText = 'Prueba gratis';
+    if (props.language === 'EN') {
       buttonText = 'Try for free';
     } else {
       buttonText = 'Prueba gratis';
     }
 
-    return (
-      <Navbar className = {this.props.className}>
-          <DesktopMenu>
-            <Link to="/">
-              <img src={logo} alt="logo" style={iconStyle}/>
-            </Link>
-            <Logo>Envoy English</Logo>
-            <NavMenu/>
-          </DesktopMenu>
-          <HamburgerMenu>
-          <MobileMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
-            <FontAwesomeIcon color='black' size="2x" icon={faBars} id="page-wrap"/>
-          </HamburgerMenu>
-        <NavbarButtons>
-          {this.props.page !== 'free-trial' &&
-            <Link to='/register'><Button primary id="register">{buttonText}</Button></Link>
-          }
-          {this.props.page === 'briefing' &&
-            <a target='_blank' rel='noopener noreferrer' href={this.props.pdf}>
-              <Button primary download>
-                {this.props.language === 'EN' ? 'Download as PDF' : 'Descarger en PDF'}
-              </Button>
-            </a>
-          }
-          <FormControl variant="outlined">
-            <Select style={{width: '70px'}}
-              value={this.props.language}
-              onChange={(e) => this.props.handleSetLanguage(e.target.value)}
-            >
-              <MenuItem value={'EN'}>EN</MenuItem>
-              <MenuItem value={'ES'}>ES</MenuItem>
-            </Select>
-          </FormControl>
-        </NavbarButtons>
-      </Navbar>
-    );
-  }
-};
+  return (
+    <Navbar className = {props.className}>
+        <DesktopMenu>
+          <Link to="/">
+            <img src={logo} alt="logo" style={iconStyle}/>
+          </Link>
+          <Logo>Envoy English</Logo>
+          <NavMenu/>
+        </DesktopMenu>
+        <HamburgerMenu>
+        <MobileMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
+          <FontAwesomeIcon color='black' size="2x" icon={faBars} id="page-wrap"/>
+        </HamburgerMenu>
+      <NavbarButtons>
+        {props.page !== 'free-trial' &&
+          <Link to='/register'><Button primary id="register">{buttonText}</Button></Link>
+        }
+        {props.page === 'briefing' &&
+          <a target='_blank' rel='noopener noreferrer' href={props.pdf}>
+            <Button primary download>
+              {props.language === 'EN' ? 'Download as PDF' : 'Descarger en PDF'}
+            </Button>
+          </a>
+        }
+        <FormControl variant="outlined">
+          <Select style={{width: '70px'}}
+            value={props.language}
+            onChange={(e) => props.handleSetLanguage(e.target.value)}
+          >
+            <MenuItem value={'EN'}>EN</MenuItem>
+            <MenuItem value={'ES'}>ES</MenuItem>
+          </Select>
+        </FormControl>
+      </NavbarButtons>
+    </Navbar>
+  );
+}
 
 export default Header;
